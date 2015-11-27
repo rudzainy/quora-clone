11ed55423128623fe8dbbc2874e3f3a534cf169c
Rudzainy Rahman <rudzainy@gmail.com>
2015-11-27 14:47:03 +0800

    Improve seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
-
+# Create default user
 User.create(name: 'Tommy Middlefinger', email: 'tommy@gmail.com', password: '123456', description: '50% Jedi, 50% Sith, 100% Vulcan', avatar: "http://localhost:9393/img/mabul.jpg")
 
+# Seed 50 random users
 50.times do
 	description = Faker::Name.title + ", " + Faker::Company.bs.capitalize
 	User.create(name: Faker::Name.name, email: Faker::Internet.email, password: '123456', description: description, avatar: Faker::Avatar.image)
 end
 
+# Seed 75 random questions
 75.times do
 	body = ""
 	rand(1..3).times do
@@ -14,6 +16,7 @@ end
 	Question.create(body: body, user_id: rand(1..51))
 end
 
+# Seed 200 random answers
 200.times do
 	body = ""
 	rand(1..10).times do
@@ -22,6 +25,19 @@ end
 	Answer.create(body: body, question_id: rand(1..75), user_id: rand(1..51))
 end
 
+# Fix exclamation mark in questions
+Question.all.each do |question|
+	question.body = question.body.gsub('!', '.')
+	question.save
+end
+
+# Fix exclamation mark in answers
+Answer.all.each do |answer|
+	answer.body = answer.body.gsub('!', '.')
+	answer.save
+end
+
+# Seed votes to questions and answers
 User.all.each do |user|
 	Question.all.each do |question|
 		if rand(3) != 1
