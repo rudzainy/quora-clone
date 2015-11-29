15ee228d7470ba5f4eee3b4f0f10d4ad7380f958
Rudzainy Rahman <rudzainy@gmail.com>
2015-11-29 20:41:07 +0800

    Setup public folder path in config/init

--- config/environments/init.rb
+++ config/environments/init.rb
@@ -53,3 +53,6 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 # Setup controller (C)
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 ######################################################################
+
+# Setup public folder
+set :public_folder, File.join(APP_ROOT, "public")
